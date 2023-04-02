2e2013dc1ff39d58449277d59b118c265b4286fd
unknown <louiecervantes@wvsu.edu.ph>
2023-04-02 20:22:38 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,14 @@ def app():
     st.write("The NLP Demo App demonstrates various concepts in Natural Language Processing")
                  
     st.write("Natural Language Processing (NLP) is a subfield of artificial intelligence that deals with the interaction between computers and human languages. NLP involves the use of various computational techniques to enable machines to process, analyze, and understand natural language data such as text, speech, and other forms of communication. NLP can be used for a wide range of applications, including language translation, sentiment analysis, speech recognition, and chatbots. Some of the key techniques used in NLP include text preprocessing, machine learning, deep learning, and statistical analysis. Overall, NLP aims to bridge the gap between human communication and machine understanding, enabling machines to interact with humans in a more natural and intuitive way.")
+    st.subheader('Tokenizer')
+    st.write('Copy the sample sentence and paste in the input box.)
+    st.write('From a dream a university grew, Stirring hearts with a heroes refrain. \
+            Out of darkness faith renew That our toils should not fade in vain. \
+            West Visayas State University, Hold thy banner high, \
+            Let genius bridge the earth And boundless sky. \
+            Set the youth to task half begun, \
+            Seek their rightful place ‘neath the sun.')
 
  
 # run the app
