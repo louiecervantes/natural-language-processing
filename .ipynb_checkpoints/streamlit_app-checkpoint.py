8742c1f98ffd1340ee4aa6a9162a463c070dd592
unknown <louiecervantes@wvsu.edu.ph>
2023-04-09 11:52:22 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -170,16 +170,44 @@ def app():
             'WRB wh-adverb, eg- where, when']
     for item in meaning:
         st.write(item)
-        
-    user_input = st.text_input("Enter a sentence")
+    
+    # POS_TAGGER_FUNCTION : TYPE 1
+    def pos_tagger(nltk_tag):
+        if nltk_tag.startswith('J'):
+            return wordnet.ADJ
+        elif nltk_tag.startswith('V'):
+            return wordnet.VERB
+        elif nltk_tag.startswith('N'):
+            return wordnet.NOUN
+        elif nltk_tag.startswith('R'):
+            return wordnet.ADV
+        else:         
+            return None
+    
+    user_input = st.text_input('Enter a sentence', 'the cat is sitting with the bats on the striped mat under many flying geese')
     pos_tags = []
-    if st.button("POS"):
-        from nltk.tokenize import word_tokenize
-        tokens = word_tokenize(user_input)
-        for i in tokens:
-            pos_tags.append(nltk.pos_tag([i]))
-        st.write(pos_tags)
-     
+    with st.echo(code_location='below'): 
+        if st.button("get POS tags"):
+            from nltk.tokenize import word_tokenize
+            # tokenize the sentence and find the POS tag for each token
+            st.write('POS tagged:')
+            pos_tagged = nltk.pos_tag(nltk.word_tokenize(user_input)) 
+            st.write(pos_tagged)
+            st.write('Wordnet tagged:')
+            wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), pos_tagged))
+            print(wordnet_tagged)
+            lemmatized_sentence = []
+            for word, tag in wordnet_tagged:
+                if tag is None:
+                    # if there is no available tag, append the token as is
+                    lemmatized_sentence.append(word)
+                else:       
+                    # else use the tag to lemmatize the token
+                    lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
+            lemmatized_sentence = " ".join(lemmatized_sentence)
+
+            st.write(lemmatized_sentence)        
+
 
  
 # run the app
